1199362b60febba9b69738c672e814eb9e12b3a7
GeraldRyan <gerald.ryan40@protonmail.com>
2021-03-08 15:33:02 -0600

    Got message digest object created

--- Java BE/Case Study/src/privblock/gerald/ryan/Block.java
+++ Java BE/Case Study/src/privblock/gerald/ryan/Block.java
@@ -1,5 +1,7 @@
 package privblock.gerald.ryan;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.time.Instant;
 import java.util.Date; // gets time in ms. 
 
@@ -19,6 +21,22 @@ public class Block {
 	}
 
 	public static void main(String[] args) {
-
+		String s = "foo";
+		System.out.println(s);
+		MessageDigest md;
+		try {
+			md = MessageDigest.getInstance("SHA-256");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			System.err.println("I'm sorry, but MD5 is not a valid message digest algorithm");
+			e.printStackTrace();
+			return;
+		}
+		System.out.println(md);
 	}
+
+//	
+//	getHash256(String s){
+//		
+//	}
 }
