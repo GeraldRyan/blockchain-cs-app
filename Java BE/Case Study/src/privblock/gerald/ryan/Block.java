12a10ef8c83bf495846bf1fb710e711fa07daf46
GeraldRyan <gerald.ryan40@protonmail.com>
2021-03-16 18:48:27 -0500

    Got blockchain completed not tested fully yet but to come

--- Java BE/Case Study/src/privblock/gerald/ryan/Block.java
+++ Java BE/Case Study/src/privblock/gerald/ryan/Block.java
@@ -22,7 +22,12 @@ public class Block {
 	String[] data;
 	int difficulty;
 	int nonce;
-
+	static int GENESIS_DIFFICULTY = 10;
+	static String[] GENESIS_DATA = new String[] {"Dance","The", "Waltz"};
+	static String GENESIS_HASH = "Genesis_Hash";
+	static String GENESIS_LAST_HASH = "Genesis_Last_Hash";
+	
+	
 // Nanosecond basis
 //	static long NANOSECONDS = 1;
 //	static long MICROSECONDS = 1000 * NANOSECONDS;
@@ -58,8 +63,15 @@ public class Block {
 	}
 
 	public String toString() {
+		String datastring = "";
+		for (String s: data) {
+			datastring = datastring + s + "|--|";
+		}
+		
 		return "\n-----------BLOCK--------\ntimestamp: " + this.timestamp + "\nlastHash: " + this.lastHash + "\nhash: "
-				+ this.hash + "\ndifficulty: " + this.getDifficulty() + "\nNonce: " + this.nonce
+				+ this.hash + "\ndifficulty: " + this.getDifficulty() + "\nData: " + 
+				datastring + 
+				"\nNonce: " + this.nonce
 				+ "\n-----------------------\n";
 	}
 
@@ -109,10 +121,10 @@ public class Block {
 	 */
 	public static Block genesis_block() {
 		long timestamp = 1;
-		String last_hash = "genesis_last_hash";
-		String hash = "genesis_hash";
-		String[] data = { "buy", "privcoin" };
-		int difficulty = 5;
+		String last_hash = GENESIS_LAST_HASH;
+		String hash = GENESIS_HASH;
+		String[] data = GENESIS_DATA;
+		int difficulty = GENESIS_DIFFICULTY;
 		int nonce = 0;
 		return new Block(timestamp, last_hash, hash, data, difficulty, nonce);
 	}
@@ -212,8 +224,7 @@ public class Block {
 //		String md = CryptoHash.getSHA256("foobar");
 		Block genesis = genesis_block();
 		System.out.println(genesis.toString());
-//		Block bad_block = Block.mine_block(genesis, new String[] { "watch", "AOT" });
-//		bad_block.lastHash = "evil data";
+
 //		System.out.println(bad_block.toString());
 		Block good_block = mine_block(genesis, new String[] { "foo", "bar" });
 		System.out.println(good_block.toString());
@@ -224,8 +235,22 @@ public class Block {
 		Block good_block2 = mine_block(good_block, new String[] { "bar", "foo" });
 		Block good_block3 = mine_block(good_block2, new String[] { "bar", "foo" });
 		Block good_block4 = mine_block(good_block3, new String[] { "bar", "foo" });
-//		Block good_block5 = mine_block(good_block4, new String[] {"bar", "foo"});
-//		Block good_block6 = mine_block(good_block5, new String[] {"bar", "foo"});
+		Block good_block5 = mine_block(good_block4, new String[] {"bar", "foo"});
+		Block good_block6 = mine_block(good_block5, new String[] {"bar", "foo"});
+		Block good_block7 = mine_block(good_block6, new String[] { "bar", "foo" });
+		Block good_block8 = mine_block(good_block7, new String[] { "bar", "foo" });
+		Block good_block9 = mine_block(good_block8, new String[] { "bar", "foo" });
+		Block good_block10 = mine_block(good_block9, new String[] {"bar", "foo"});
+		Block good_block11 = mine_block(good_block10, new String[] {"bar", "foo"});
+		Block bad_block1 = Block.mine_block(genesis, new String[] { "zeke", "yeager" });
+		bad_block1.lastHash = "evil data";
+		System.out.println(Block.is_valid_block(bad_block1, genesis));
+		Block bad_block2 = mine_block(good_block9, new String[] {"hello", "Lviv"});
+		System.out.println(Block.is_valid_block(bad_block2, good_block8));
+		
+		//		for (int i = 0; i< 10; i++) {
+//			Block i = mine_block(good_block[i-1])
+//		}
 	}
 
 }
--- Java BE/Case Study/src/privblock/gerald/ryan/Blockchain.java
+++ Java BE/Case Study/src/privblock/gerald/ryan/Blockchain.java
@@ -1,19 +1,99 @@
 package privblock.gerald.ryan;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+
 import privblock.gerald.ryan.Block;
+
+
 /**
  * 
- * @author Gerald Ryan
- *Blockchain class of blockchain app.
+ * @author Gerald Ryan Blockchain class of blockchain app.
  *
  *
  *
  */
 
 public class Blockchain {
-//	
-	public Block create_genesis() {
-		Block genesis_block = new Block(1, "genesis_last_hash",);
-		return genesis_block;
+	ArrayList<Block> chain = new ArrayList<Block>();
+
+	/**
+	 * Constructor function, initializes an array list and adds a genesis block
+	 */
+	Blockchain() {
+		this.chain.add(Block.genesis_block());
+	}
+
+	public void add_block(String[] data) throws NoSuchAlgorithmException {
+		Block new_block = Block.mine_block(this.chain.get(this.chain.size() - 1), data);
+		this.chain.add(new_block);
+	}
+
+	/**
+	 * Replace the local chain with the incoming chain if the following apply: - the
+	 * incoming chain is longer than the local one - the incoming chain is formatted
+	 * properly
+	 * 
+	 * @param chain
+	 * @throws NoSuchAlgorithmException 
+	 */
+	public void replace_chain(Blockchain other_blockchain) throws NoSuchAlgorithmException {
+		if (other_blockchain.chain.size() <= this.chain.size()) {
+			System.out.println("Cannot replace chain. The incoming chain must be longer");
+			return;
+		} 
+		else if (!Blockchain.is_valid_chain(other_blockchain)) {
+			System.out.println("Cannot replace chain. The incoming chain is invalid");
+			return;
+		}
+		
+		this.chain = other_blockchain.chain;
+//		else {
+//			try {
+//				Blockchain.is_valid_chain(other_blockchain);
+//			} catch (Exception e) {
+//				throw new // exception
+//				// block of code to handle errors
+//			}
+//		}
+	}
+
+	/**
+	 * Validate the incoming chain. Enforce the following rules: - the chain must
+	 * start with the genesis block - blocks must be formatted correctly
+	 * 
+	 * @param blockchain
+	 * @return
+	 * @throws NoSuchAlgorithmException
+	 */
+	public static boolean is_valid_chain(Blockchain blockchain) throws NoSuchAlgorithmException {
+		if (blockchain.chain.get(0) != Block.genesis_block()) {
+			System.out.println("The genesis block must be valid");
+			return false;
+			// throw exception: the genesis block must be valid
+		}
+		for (int i = 1; i < blockchain.chain.size(); i++) {
+			Block current_block = blockchain.chain.get(i);
+			Block last_block = blockchain.chain.get(i - 1);
+			if (!Block.is_valid_block(last_block, current_block)) {
+				System.out.println("At least one of the blocks in the chain is not valid");
+				return false;
+				// throw exception at least one of the blocks in the chain is not valid
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Blockchain: " + this.chain;
+	}
+	
+	public static void main(String[] args) throws NoSuchAlgorithmException {
+		Blockchain blockchain = new Blockchain();
+//		System.out.println(blockchain);
+		blockchain.add_block(new String[] {"Shakespeare", "wrote", "it"});
+		System.out.println(blockchain);
 	}
 
 }
