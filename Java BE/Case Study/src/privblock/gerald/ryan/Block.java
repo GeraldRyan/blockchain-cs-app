3bc87245fb954b655a8556c6e04ca45ec60c2da1
GeraldRyan <gerald.ryan40@protonmail.com>
2021-03-16 16:46:20 -0500

    Got block proof of work and mine_block working

--- Java BE/Case Study/src/privblock/gerald/ryan/Block.java
+++ Java BE/Case Study/src/privblock/gerald/ryan/Block.java
@@ -3,6 +3,7 @@ package privblock.gerald.ryan;
 import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.Date; // gets time in ms.
 import privblock.gerald.ryan.util.CryptoHash;
 
@@ -22,11 +23,19 @@ public class Block {
 	int difficulty;
 	int nonce;
 
-	static long NANOSECONDS = 1;
-	static long MICROSECONDS = 1000 * NANOSECONDS;
-	static long MILLISECONDS = 1000 * MICROSECONDS;
+// Nanosecond basis
+//	static long NANOSECONDS = 1;
+//	static long MICROSECONDS = 1000 * NANOSECONDS;
+//	static long MILLISECONDS = 1000 * MICROSECONDS;
+//	static long SECONDS = 1000 * MILLISECONDS;
+//	static long MINE_RATE = 4 * SECONDS;
+//	static long NANOSECONDS = 1;
+
+// Millisecond basis
+	;
+	static long MILLISECONDS = 1;
 	static long SECONDS = 1000 * MILLISECONDS;
-	static long MINE_RATE = 4 * SECONDS;
+	static long MINE_RATE = 2 * SECONDS;
 
 	/**
 	 * A block is a unit of storage for a blockchain that supports a cryptocurrency.
@@ -48,6 +57,12 @@ public class Block {
 		this.nonce = nonce;
 	}
 
+	public String toString() {
+		return "\n-----------BLOCK--------\ntimestamp: " + this.timestamp + "\nlastHash: " + this.lastHash + "\nhash: "
+				+ this.hash + "\ndifficulty: " + this.getDifficulty() + "\nNonce: " + this.nonce
+				+ "\n-----------------------\n";
+	}
+
 	/**
 	 * Mine a block based on given last block and data until a block hash is found
 	 * that meets the leading 0's Proof of Work requirement.
@@ -62,7 +77,43 @@ public class Block {
 		String last_hash = last_block.getHash();
 		int difficulty = Block.adjust_difficulty(last_block, timestamp);
 		int nonce = 0;
-		String hash = CryptoHash.getSHA256(new Date().getTime(), last_block.getHash(), data, difficulty, nonce);
+		String hash = CryptoHash.getSHA256(timestamp, last_block.getHash(), data, difficulty, nonce);
+
+		String proof_of_work = CryptoHash.n_len_string('0', difficulty);
+		String binary_hash = CryptoHash.hex_to_binary(hash);
+		String binary_hash_work_end = binary_hash.substring(0, difficulty);
+		System.out.println("Difficulty: " + difficulty);
+		System.out.println("Working");
+		while (!proof_of_work.equalsIgnoreCase(binary_hash_work_end)) {
+			nonce += 1;
+			timestamp = new Date().getTime();
+			difficulty = Block.adjust_difficulty(last_block, timestamp);
+			hash = CryptoHash.getSHA256(timestamp, last_block.getHash(), data, difficulty, nonce);
+			proof_of_work = CryptoHash.n_len_string('0', difficulty);
+			binary_hash = CryptoHash.hex_to_binary(hash);
+			binary_hash_work_end = binary_hash.substring(0, difficulty);
+		}
+		System.out.println("Solved at Difficulty: " + difficulty);
+		System.out.println("Proof of work requirement " + proof_of_work);
+		System.out.println("binary_Hash_work_end " + binary_hash_work_end);
+		System.out.println("binary hash " + binary_hash);
+		System.out.println("BLOCK MINED");
+
+		return new Block(timestamp, last_hash, hash, data, difficulty, nonce);
+	}
+
+	/**
+	 * Generate Genesis block
+	 * 
+	 * @return
+	 */
+	public static Block genesis_block() {
+		long timestamp = 1;
+		String last_hash = "genesis_last_hash";
+		String hash = "genesis_hash";
+		String[] data = { "buy", "privcoin" };
+		int difficulty = 5;
+		int nonce = 0;
 		return new Block(timestamp, last_hash, hash, data, difficulty, nonce);
 	}
 
@@ -76,43 +127,18 @@ public class Block {
 	 */
 	public static int adjust_difficulty(Block last_block, long new_timestamp) {
 		long time_diff = new_timestamp - last_block.getTimestamp();
+
+//		System.out.println(time_diff);
 		if (time_diff < MINE_RATE) {
+//			System.out.println("Increasing difficulty");
 			return last_block.getDifficulty() + 1;
 		} else if (last_block.getDifficulty() - 1 > 0) {
+//			System.out.println("Decreasing difficulty");
 			return last_block.getDifficulty() - 1;
 		} else {
 			return 1;
 		}
 	}
-
-	public int getDifficulty() {
-		return difficulty;
-	}
-
-	public long getTimestamp() {
-		return timestamp;
-	}
-
-	public static Block genesis_block() {
-		long timestamp = 1;
-		String last_hash = "genesis_last_hash";
-		String hash = "genesis_hash";
-		String[] data = { "buy", "privcoin" };
-		int difficulty = 3;
-		int nonce = 0;
-		return new Block(timestamp, last_hash, hash, data, difficulty, nonce);
-	}
-
-	public String getHash() {
-		return hash;
-	}
-
-	/**
-	 * Description -
-	 * 
-	 * @param args
-	 * @throws NoSuchAlgorithmException
-	 */
 
 	/**
 	 * Validate block by enforcing following rules: - Block must have the proper
@@ -123,34 +149,83 @@ public class Block {
 	 * @param last_block
 	 * @param block
 	 * @return
+	 * @throws NoSuchAlgorithmException
 	 */
-	public static boolean is_valid_block(Block last_block, Block block) {
-		if (block.getHash() != last_block.getHash()) {
+	public static boolean is_valid_block(Block last_block, Block block) throws NoSuchAlgorithmException {
+		String binary_hash = CryptoHash.hex_to_binary(block.getHash());
+		char[] pow_array = CryptoHash.n_len_array('0', block.getDifficulty());
+		char[] binary_char_array = CryptoHash.string_to_charray(binary_hash);
+		if (!block.getLastHash().equalsIgnoreCase(last_block.getHash())) {
+			System.out.println("The last hash must be correct");
 			return false;
-			// Should I throw an exception?
+			// Throw exception the last hash must be correct
 		}
-
-		else {
-			return true;
+		if (!Arrays.equals(pow_array, Arrays.copyOfRange(binary_char_array, 0, block.getDifficulty()))) {
+			System.out.println("Proof of work requirement not met");
+			return false;
+			// throw exception - proof of work requirement not met
 		}
+		if (Math.abs(last_block.difficulty - block.difficulty) > 1) {
+			System.out.println("Block difficulty must adjust by one");
+			return false;
+			// throw exception: The block difficulty must only adjust by 1
+		}
+		String reconstructed_hash = CryptoHash.getSHA256(block.getTimestamp(), block.getLastHash(), block.getData(),
+				block.getDifficulty(), block.getNonce());
+		if (!block.getHash().equalsIgnoreCase(reconstructed_hash)) {
+			System.out.println("The block hash must be correct");
+			System.out.println(block.getHash());
 
+			System.out.println(reconstructed_hash);
+			return false;
+			// throw exception: the block hash must be correct
+		}
+		System.out.println("You have mined a valid block");
+		return true;
 	}
 
-	public String toString() {
-		return "\n-----------BLOCK--------\ntimestamp: " + this.timestamp + "\nlastHash: " + this.lastHash + "\nhash: "
-				+ this.hash + "\ndifficulty: " + this.getDifficulty() + "\nNonce: " + this.nonce
-				+ "\n-----------------------\n";
+	public int getDifficulty() {
+		return difficulty;
+	}
 
+	public long getTimestamp() {
+		return timestamp;
+	}
+
+	public String getHash() {
+		return hash;
+	}
+
+	public String getLastHash() {
+		return lastHash;
+	}
+
+	public String[] getData() {
+		return data;
+	}
+
+	public int getNonce() {
+		return nonce;
 	}
 
 	public static void main(String[] args) throws NoSuchAlgorithmException {
-		String md = CryptoHash.getSHA256("foobar");
+//		String md = CryptoHash.getSHA256("foobar");
 		Block genesis = genesis_block();
 		System.out.println(genesis.toString());
-		String[] new_data = { "watch", "AOT" };
-		Block new_block = mine_block(genesis, new_data);
-		System.out.println(new_block.toString());
-		System.out.println(mine_block(new_block, new String[] { "crypto", "is", "fun" }).toString());
+//		Block bad_block = Block.mine_block(genesis, new String[] { "watch", "AOT" });
+//		bad_block.lastHash = "evil data";
+//		System.out.println(bad_block.toString());
+		Block good_block = mine_block(genesis, new String[] { "foo", "bar" });
+		System.out.println(good_block.toString());
+//		System.out.println(mine_block(new_block, new String[] { "crypto", "is", "fun" }).toString());
+//		System.out.println(Block.is_valid_block(genesis, bad_block)); // returns false as expected
+		System.out.println(Block.is_valid_block(genesis, good_block));
+		System.out.println(CryptoHash.hex_to_binary(good_block.getHash()));
+		Block good_block2 = mine_block(good_block, new String[] { "bar", "foo" });
+		Block good_block3 = mine_block(good_block2, new String[] { "bar", "foo" });
+		Block good_block4 = mine_block(good_block3, new String[] { "bar", "foo" });
+//		Block good_block5 = mine_block(good_block4, new String[] {"bar", "foo"});
+//		Block good_block6 = mine_block(good_block5, new String[] {"bar", "foo"});
 	}
 
 }
