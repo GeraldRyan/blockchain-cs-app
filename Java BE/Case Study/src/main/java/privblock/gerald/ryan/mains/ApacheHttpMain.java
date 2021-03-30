598f151df08e394a8e4063dd61a04f9d3b78ce05
GeraldRyan <gerald.ryan40@protonmail.com>
2021-03-30 10:15:16 -0500

    Got apache http set up and returnign raw response data

---
+++ Java BE/Case Study/src/main/java/privblock/gerald/ryan/mains/ApacheHttpMain.java
@@ -0,0 +1,36 @@
+package privblock.gerald.ryan.mains;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+public class ApacheHttpMain {
+
+	public static void main(String[] args) {
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		HttpGet httpGet = new HttpGet("http://localhost:8080/CaseStudy/blockchain/");
+		CloseableHttpResponse response1;
+		try {
+			response1 = httpclient.execute(httpGet);
+			System.out.println(response1.getStatusLine());
+//			System.out.println(response1.toString());
+			HttpEntity entity1 = response1.getEntity();
+			Scanner sc = new Scanner(entity1.getContent());
+			while (sc.hasNext()) {
+				System.out.println(sc.nextLine());
+			}
+			System.out.println(entity1.getContent());
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+//			e.printStackTrace();
+		}
+
+	}
+
+}
